64abaebdd0dadbbfeb87cf0f19fa0e4258fa65ef
santiago <tolaba.santiago.dm@gmail.com>
2026-04-14 17:22:17 -0300

    sentencia tp chinook

---
+++ sqlagbd1.sql
@@ -0,0 +1,7 @@
+---- EJERCICIO 1 -----------------------------------------
+
+-- SELECT FirstName as nombre, LastName as apellido 
+-- FROM employees
+-- ORDER by FirstName, LastName
+
+-----------------------------------------------------------
